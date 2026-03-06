c8133421dec4b3d2e06572a71cfba6cde018e3b2
Swastik Bose <cpswastik31@gmail.com>
2026-03-06 12:57:45 +0530

    task : fixed prisma config and tested

--- DevAll/apps/auth-service/src/app/auth.controller.ts
+++ DevAll/apps/auth-service/src/app/auth.controller.ts
@@ -1,35 +1,13 @@
-import { Controller } from '@nestjs/common';
-import { MessagePattern, Payload } from '@nestjs/microservices';
+import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
-import { CreateAuthDto } from './dto/create-auth.dto';
-import { UpdateAuthDto } from './dto/update-auth.dto';
+import { type User } from '@dev-all/database';
 
-@Controller()
+@Controller('auth')
 export class AuthController {
-  constructor(private readonly authService: AuthService) {}
+  constructor(private readonly authService: AuthService) { }
 
-  @MessagePattern('createAuth')
-  create(@Payload() createAuthDto: CreateAuthDto) {
-    return this.authService.create(createAuthDto);
-  }
-
-  @MessagePattern('findAllAuth')
-  findAll() {
-    return this.authService.findAll();
-  }
-
-  @MessagePattern('findOneAuth')
-  findOne(@Payload() id: number) {
-    return this.authService.findOne(id);
-  }
-
-  @MessagePattern('updateAuth')
-  update(@Payload() updateAuthDto: UpdateAuthDto) {
-    return this.authService.update(updateAuthDto.id, updateAuthDto);
-  }
-
-  @MessagePattern('removeAuth')
-  remove(@Payload() id: number) {
-    return this.authService.remove(id);
+  @Post('signup')
+  async signup(@Body() body: User) {
+    this.authService.signup(body.email, body.password, body.name)
   }
 }
---
+++ DevAll/packages/database/src/index.ts
@@ -0,0 +1,2 @@
+export * from './db-nest.js'
+export * from '@prisma/client'
